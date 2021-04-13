4507991c2636527bdfaf2b9d490df8dca326e369
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-04-13 00:26:47 -0300

    Async Await e Comentários

--- controllers/series.js
+++ controllers/series.js
@@ -1,67 +1,65 @@
-// const Serie = require('../models/series')
-
 const labels = [
   { id: 'to-watch', name: 'Para assistir' },
   { id: 'watching', name: 'Assistindo' },
   { id: 'watched', name: 'Assistido' },
 ]
 
-const index = ({ Serie }, req, res) => {
-  // Serie.find({}, (err, docs) => res.send(docs))
-  // res.render('series/index')
-  Serie.find({}, (err, docs) => {
-    // res.render('series/index', { series: docs })
-    res.render('series/index', { series: docs, labels })
-  })
+const index = async ({ Serie }, req, res) => {
+  const docs = await Serie.find({})
+  res.render('series/index', { series: docs, labels })
 }
-// const nova = (req, res) => res.render('series/nova')
-// const novaProcess = ({ Serie }, req, res) => {
-//   const serie = new Serie({
-//     name: 'Friends',
-//     status: 'watched'
-//   })
-//   serie.save(() => console.log('saved'))
-//   res.render('series/nova')
-// }
-const novaProcess = ({ Serie }, req, res) => {
+
+const novaProcess = async ({ Serie }, req, res) => {
   const serie = new Serie(req.body)
-  serie.save(() => {
+  try {
+    await serie.save()
     res.redirect('/series')
-  })
+  } catch (e) {
+    // console.log(Object.keys(e.errors))
+    res.render('series/nova', {
+      errors: Object.keys(e.errors)
+    })
+  }
 }
 
 const novaForm = (req, res) => {
-  res.render('series/nova')
+  res.render('series/nova', { errors: [] })
 }
 
-const excluir = ({ Serie }, req, res) => {
-  Serie.remove({
-    _id: req.params.id
-  }, (err) => {
+const excluir = async ({ Serie }, req, res) => {
+  await Serie.remove({ _id: req.params.id })
+  res.redirect('/series')
+}
+
+const editarProcess = async ({ Serie }, req, res) => {
+  const serie = await Serie.findOne({ _id: req.params.id })
+  serie.name = req.body.name
+  serie.status = req.body.status
+  try {
+    await serie.save()
     res.redirect('/series')
-  })
+  } catch (e) {
+    res.render('series/editar', {
+      errors: Object.keys(e.errors)
+    })
+  }
 }
 
-const editarProcess = ({ Serie }, req, res) => {
-  Serie.findOne({ _id: req.params.id }, (err, serie) => {
-    serie.name = req.body.name
-    serie.status = req.body.status
-    serie.save()
-    res.redirect('/series')
-  })
+const editarForm = async ({ Serie }, req, res) => {
+  const serie = await Serie.findOne({ _id: req.params.id })
+  res.render('series/editar', { serie, labels, errors: [] })
 }
 
-const editarForm = ({ Serie }, req, res) => {
-  // const labels = [
-  //   { id: 'to-watch', name: 'Para assistir' },
-  //   { id: 'watching', name: 'Assistindo' },
-  //   { id: 'watched', name: 'Assistido' },
-  // ]
-  Serie.findOne({ _id: req.params.id }, (err, serie) => {
-    res.render('series/editar', { serie, labels })
-  })
+const info = async ({ Serie }, req, res) => {
+  const serie = await Serie.findOne({ _id: req.params.id })
+  res.render('series/info', { serie })
+}
+
+const addComentario = async ({ Serie }, req, res) => {
+  await Serie.updateOne({ _id: req.params.id }, { $push: { comments: req.body.comentario } })
+  res.redirect('/series/info/' + req.params.id)
 }
 
 module.exports = {
-  index, novaProcess, novaForm, excluir, editarForm, editarProcess
+  index, novaProcess, novaForm, excluir, editarForm, editarProcess, info, addComentario
 }
